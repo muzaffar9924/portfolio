3ca1f5568cf5d3b4956ba074ff6371568f604ed6
muzaffar9924 <muzaffar.hussain.work@gmail.com>
2025-07-28 13:16:39 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/'  // Use your repo name here
+    : '/',
   plugins: [vue()],
 })
